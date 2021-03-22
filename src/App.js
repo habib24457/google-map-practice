126f54581f0036c5ba15bf782325d512988ebc35
Habibur Rahman <rahmanhabibur24457@gmail.com>
2021-03-22 17:00:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import Map from './Components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React google map</h1>
+      <Map></Map>
+    </div>
+  );
+}
+
+export default App;
